0964fd18d33284c6928bb72b2a4c0b1aef89dd8f
VladNCode <vldsmrfow@gmail.com>
2021-11-10 13:27:10 +0200

    added yargs and started to build filter

--- app.js
+++ app.js
@@ -1,9 +1,42 @@
-const validator = require('validator');
+const yargs = require('yargs');
+const chalk = require('chalk');
 const notes = require('./notes');
+const { argv } = require('yargs');
 
-const msg = notes();
+yargs
+  .command(
+    'add',
+    'Add a new note!',
+    {
+      title: {
+        describe: 'Your note title',
+        demandOption: true,
+        type: 'string',
+      },
+      body: {
+        describe: 'Your note text',
+        demandOption: true,
+        type: 'string',
+      },
+    },
+    a => console.log(`Title: ${a.title}, Body: ${a.body}`)
+  )
+  .command(
+    'remove',
+    'Remove a note!',
+    { title: { describe: 'Note title' } },
+    a => console.log('Removing a new note', a)
+  )
+  .command('read', 'Read a note!', { title: { describe: 'Note title' } }, a =>
+    console.log('Reading a note', a)
+  )
+  .command(
+    'list',
+    'List your notes!',
+    { title: { describe: 'Note title' } },
+    a => console.log('Listing out all notes!', a)
+  )
+  .parse();
 
-console.log(msg);
-
-console.log(validator.isEmail('email@example.com'));
-console.log(validator.isURL('example.com'));
+// console.log(argv);  Show current argv
+// yargs.showHelp();   Show all commands
