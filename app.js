f989c77424ddc83270e2b3222a3f32b145ee054a
VladNCode <vldsmrfow@gmail.com>
2021-11-10 11:23:14 +0200

    Installed validator

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
+const validator = require('validator');
 const notes = require('./notes');
 
 const msg = notes();
 
 console.log(msg);
+
+console.log(validator.isEmail('email@example.com'));
+console.log(validator.isURL('example.com'));
